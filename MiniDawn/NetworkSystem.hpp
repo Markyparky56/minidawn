2f652f75a9dfb3c68b27b7d28f17338b8385beab
Dawn\Mark <mark@chavfreezone.me.uk>
2016-11-28 18:54:33 +0000

    Started defining the protocol, and partially implemented a basic send/receive set up for both TCP and UDP sockets Got a working console allocated, though whether it'll be used is another question.

--- MiniDawn/NetworkSystem.hpp
+++ MiniDawn/NetworkSystem.hpp
@@ -3,20 +3,31 @@
     A Networking System to manage a UDP and TCP connection to a remote host which will have
     authorative control over the true locations of objects in the scene.
     Will, on start up, connect via TCP to the server and syncronise with any other connected clients
-    Will then regularly (e.g. 1/20th of a second) update the server of the local users current position
+    Will then regularly (e.g. 1/20th of a second) update the server of the local user's current position
     and direction (any any other important data pertaining to the user which is can fit in the pakcet)
     Will listen for UDP packets from the server (also sent regularly) for authorative updates which it 
-    may need use to correct local data (to the best of it's ability, minimising unrealistic movements
+    may need to correct local data (to the best of it's ability, minimising unrealistic movements
     which may cause rubberbanding or breaking of reality)
     Will listen on the original TCP connection for important messages from the server, like new clients
     connecting or disconnecting, snapshots of the (global) world (as opposed to local changes).
 ***************************************************************************************************/
 
+#ifdef _DEBUG
+#include <iostream>
+#endif
 #include "UniquePtr.hpp"
+#include "SharedRef.hpp"
+#include <stack> // UDP message stack, always deal with the newest message
+#include <queue> // TCP message queue, messages received over TCP are important so deal with them in the order they arrive
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include "Channel.hpp"
+#include <thread>
 
+using pThread = UniquePtr<std::thread>;
 using boost::asio::ip::tcp;
 using boost::asio::ip::udp;
 
@@ -26,25 +37,124 @@ public:
     NetworkSystem();
     ~NetworkSystem();
 
-    void UDPSend()
+    struct UDPMessage
     {
-        boost::asio::io_service io_service;
+
+    };
+
+    struct TCPMessage
+    {
+
+    };
+
+    bool UDPInit()
+    {
         udp::resolver resolver(io_service);
         udp::resolver::query query(udp::v4(), "localhost", "4444");
-        resolver.async_resolve(query, boost::bind(&NetworkSystem::UDPEndpointResolverHandler, shared_from_this()));
-        udp::socket socket(io_service);
-        socket.open(udp::v4());
+        boost::asio::ip::basic_resolver_iterator<boost::asio::ip::udp> endpoints;
+#ifdef _DEBUG
+        try
+        {
+#endif
+            endpoints = resolver.resolve(query);
+#ifdef _DEBUG
+        }
+        catch (boost::system::system_error &e)
+        {
+            // Resolve failed
+            std::cerr << e.what() << std::endl;
+
+            abort(); // Graceful, nope, probably my own fault, likely.
+        }
+#endif
+        udpEndpoint = *endpoints;
+        udpSocket = udp::socket(io_service);
+        udpSocket.open(udp::v4()); // We need our own socket so we can listen to what the server wants to send us
+    }
+
+    void UDPSend(UDPMessage &msg)
+    {
+        memcpy(udpSendBuffer.c_array(), reinterpret_cast<uint8_t*>(&msg), sizeof(UDPMessage));
+        udpSocket.async_send_to( 
+            boost::asio::buffer(udpSendBuffer), 
+            udpEndpoint, 
+            boost::bind(&NetworkSystem::udpHandleSend, this, boost::asio::placeholders::error)
+        );
+    }
+
+    void UDPReceive()
+    {
+        udpSocket.async_receive_from(
+            boost::asio::buffer(udpRecvBuffer),
+            udpEndpoint,
+            boost::bind(&NetworkSystem::udpHandleReceive, this, boost::asio::placeholders::error)
+        );
+    }
+
+    bool TCPInit()
+    {
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(tcp::v4(), "localhost", "4444");
+        boost::asio::ip::basic_resolver_iterator<boost::asio::ip::tcp> endpoints;
+#ifdef _DEBUG
+        try
+        {
+#endif
+            endpoints = resolver.resolve(query);
+#ifdef _DEBUG
+        }
+        catch (boost::system::system_error &e)
+        {
+            // Resolve failed
+            std::cerr << e.what() << std::endl;
+            abort();
+        }
+#endif
+        tcpEndpoint = *endpoints;
+        tcpSocket = tcp::socket(io_service);
+        boost::asio::connect(tcpSocket, tcpEndpoint);
+    }
+
+    void TCPSend(TCPMessage &msg)
+    {
+        memcpy(tcpSendBuffer.c_array(), reinterpret_cast<uint8_t*>(&msg), sizeof(TCPMessage));
+        tcpSocket.async_send(
+            boost::asio::buffer(tcpSendBuffer),
+            boost::bind(&NetworkSystem::udpHandleSend, this, boost::asio::placeholders::error)
+        );
     }
 
 private:
-    void UDPEndpointResolverHandler(const boost::system::error_code &error_code, boost::asio::ip::tcp::resolver::iterator iterator)
-    {
+    void udpHandleReceive(const boost::system::error_code &error, std::size_t bytesTransferred);
+    void udpHandleSend(boost::shared_ptr<UDPMessage> msg, const boost::system::error_code &error, std::size_t bytesTransferred);
 
-    }
+    void tcpHandleReceive(const boost::system::error_code &error, std::size_t bytesTransferred);
+    void tcpHandleSend(boost::shared_ptr<TCPMessage> msg, const boost::system::error_code &error, std::size_t bytesTransferred);
+        
+    boost::array<uint8_t, sizeof(UDPMessage)> udpSendBuffer;
+    boost::array<uint8_t, sizeof(UDPMessage)> udpRecvBuffer;
+    SharedPtr<UDPMessage> udpRcvdMessagePtr;
 
+    boost::array<uint8_t, sizeof(TCPMessage)> tcpSendBuffer;
+    boost::array<uint8_t, sizeof(TCPMessage)> tcpRecvBuffer;
+    SharedPtr<TCPMessage> tcpRcvdMessagePtr;
+
+    boost::asio::io_service io_service;
     udp::resolver udpResolver;
+    udp::endpoint udpEndpoint;
     tcp::resolver tcpResolver;
+    tcp::endpoint tcpEndpoint;
     tcp::socket tcpSocket;
     udp::socket udpSocket;
+
+    // For storing received messages
+    std::stack<UDPMessage> udpMessageStack;
+    std::queue<TCPMessage> tcpMessageQueue;
+
+    // For passing received messages from the async functions to the main 
+    Channel<UDPMessage> udpMessageChannel;
+    Channel<TCPMessage> tcpMessageChannel;
+
+    pThread ioServiceThread;
 };
 using pNetworkSystem = UniquePtr<NetworkSystem>;
--- MiniDawn/Source/WinMain.cpp
+++ MiniDawn/Source/WinMain.cpp
@@ -1,9 +1,16 @@
 #include <Windows.h>
+#include <cstdio>
+#include <iostream>
 
 extern int DawnMain(const TCHAR* CmdLine, HINSTANCE hInstance, HINSTANCE hPrevInstance, int nCmdShow);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nCmdShow)
 {
+    // This'll be useful for a slightly less weird debug output when dealing with network messages
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+
     const TCHAR* CmdLine = GetCommandLine();
     int Error = DawnMain(CmdLine, hInstance, hPrevInstance, nCmdShow);
     if (Error)
